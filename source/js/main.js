6f08474b0ed70b949b87be3617a166344f74d012
Vitaliy Vaskin <vital0101@yandex.ru>
2022-08-03 17:29:34 +0300

    add fix

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
-import {btnActive, menuJs} from './modules/button-menu/button-menu';
+import {swithBtn} from './modules/button-menu/button-menu';
 
 // ---------------------------------
 
@@ -14,8 +14,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
-  btnActive();
-  menuJs();
+  swithBtn();
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
--- source/js/modules/button-menu/button-menu.js
+++ source/js/modules/button-menu/button-menu.js
@@ -4,9 +4,19 @@ const pageHeader = document.querySelector('.page-header');
 const headerNav = document.querySelector('.header-navigation');
 const headerNavItem = headerNav.children;
 const siteNav = document.querySelector('.page-header__site-nav');
+const contacts = document.querySelector('.contacts__map');
+
+siteNav.classList.remove('page-header__site-nav');
+siteNav.classList.add('page-header__site-nav-js');
+
+contacts.classList.add('contacts__map--hidden');
+
+btnMenu.classList.remove('btn-menu');
+btnMenu.classList.add('btn-menu__js');
+btnMenu.classList.add('btn-menu__js--opened');
 
 
-const menuJs = () => {
+const addNewClass = () => {
   if (window.screen.width <= 767) {
     for (let i = 0; i < headerNavItem.length; i++) {
       if (headerNavItem[i].classList.contains('header-navigation__item')) {
@@ -17,21 +27,15 @@ const menuJs = () => {
   }
 };
 
-siteNav.classList.remove('page-header__site-nav');
-siteNav.classList.add('page-header__site-nav-js');
-
-btnMenu.classList.remove('btn-menu');
-btnMenu.classList.add('btn-menu__js');
-btnMenu.classList.add('btn-menu__js--opened');
-
-const btnActive = () => {
+const swithBtn = () => {
   btnMenu.addEventListener('click', function () {
     btnMenu.classList.toggle('btn-menu__js--opened');
     btnMenu.classList.toggle('btn-menu__js--closed');
     logo.classList.toggle('logo-js');
     pageHeader.classList.toggle('page-header--bg');
     siteNav.classList.toggle('page-header__site-nav-js--active');
+    addNewClass();
   });
 };
 
-export {btnActive, menuJs};
+export {swithBtn};
